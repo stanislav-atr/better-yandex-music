ec4534258a0a7cb863dbd035b47843b284c6f246
Stanislav A <s.atroschenko@adguard.com>
2022-06-18 19:35:23 +0300

    add default style params

--- src/better-lyrics/LyricsWindow/LyricsWindow.jsx
+++ src/better-lyrics/LyricsWindow/LyricsWindow.jsx
@@ -7,9 +7,19 @@ import { STYLES } from '../../utils/constants';
 import './lyrics-window.css';
 
 const LyricsWindow = () => {
+    const defaultStyleParams = {
+        width: 360,
+        height: 640,
+        minWidth: '300',
+        minHeight: '533',
+        fontSize: '15',
+        firstLineMargin: '5%',
+        lastLineMargin: '5%',
+    };
     const [lyrics, setLyrics] = useState('DEFAULT GREETING TEXT');
-    const [fontSize, setFontSize] = useState('15');
+    const [fontSize, setFontSize] = useState(defaultStyleParams.fontSize);
     const rdWrapperRef = useRef();
+
     useMusicApi(setLyrics);
 
     useEffect(() => {
@@ -20,14 +30,14 @@ const LyricsWindow = () => {
         rObserver.observe(rdWrapperRef.current.resizableElement.current);
     }, [setFontSize]);
 
-    const renderLine = (line, isFirst, isLast) => {
+    const renderLine = (line, margins) => {
         const key = (Math.random()).toString();
         const isText = line.length !== 0;
 
         const lineStyle = {
             'marginLeft': '5%',
-            'marginTop': isFirst ? '5%' : '0',
-            'marginBottom': isLast ? '5%' : '0',
+            'marginTop': margins.top,
+            'marginBottom': margins.bottom,
             'fontSize': `${fontSize}px`,
         };
 
@@ -50,18 +60,21 @@ const LyricsWindow = () => {
             default={{
                 x: 0,
                 y: 0,
-                width: 360,
-                height: 640,
+                width: defaultStyleParams.width,
+                height: defaultStyleParams.height,
             }}
-            minWidth="300"
-            minHeight="533"
+            minWidth={defaultStyleParams.minWidth}
+            minHeight={defaultStyleParams.minHeight}
         >
             <div className="lyrics_window">
                 <div className="text_box">
                     {lyrics.split('\n').map((line, i, array) => {
-                        const isFirst = i === 0;
-                        const isLast = i === array.length - 1;
-                        return renderLine(line, isFirst, isLast);
+                        const { firstLineMargin, lastLineMargin } = defaultStyleParams;
+                        const margins = {
+                            top: i === 0 ? firstLineMargin : '0',
+                            bottom: i === array.length - 1 ? lastLineMargin : '0',
+                        };
+                        return renderLine(line, margins);
                     })}
                 </div>
             </div>
